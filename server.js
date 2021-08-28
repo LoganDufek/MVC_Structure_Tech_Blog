1dd5121980ec0d9b73da02885350b4eda26e09e4
Logan D <logandufek@gmail.com>
2021-08-28 12:54:15 -0400

    heroku support added to connection.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
-
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3005;
